311960f816ed40c4ed82810dd16f13d8a74596b7
Christian Hopps <chopps@labn.net>
2023-07-09 16:55:10 -0400

    lib: yang: restore old API use now that it's fixed
    This also avoids a bug in the workaround function if the set variable
    wasn't set to NULL the Debug version of libyang would sigsegv.
    
    Signed-off-by: Christian Hopps <chopps@labn.net>

--- lib/yang.c
+++ lib/yang.c
@@ -254,8 +254,13 @@ void yang_snode_get_path(const struct lysc_node *snode,
 struct lysc_node *yang_find_snode(struct ly_ctx *ly_ctx, const char *xpath,
 				  uint32_t options)
 {
+#if 1
 	struct lysc_node *snode;
-	struct ly_set *set;
+
+	snode = (struct lysc_node *)lys_find_path(ly_ctx, NULL, xpath, 0);
+#else
+	struct lysc_node *snode;
+	struct ly_set *set = NULL;
 	LY_ERR err;
 
 	err = lys_find_xpath(ly_native_ctx, NULL, xpath, options, &set);
@@ -264,6 +269,7 @@ struct lysc_node *yang_find_snode(struct ly_ctx *ly_ctx, const char *xpath,
 
 	snode = set->snodes[0];
 	ly_set_free(set, NULL);
+#endif
 
 	return snode;
 }
