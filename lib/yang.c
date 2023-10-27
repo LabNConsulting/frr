139970c7a84859d8719623d8f6df216bca0d8e6a
Christian Hopps <chopps@labn.net>
2023-10-26 23:54:34 -0400

    Revert "lib: yang: restore old API use now that it's fixed"
    This reverts commit 48941fe9753719e0b171c9692c5d0aade6961126.

--- lib/yang.c
+++ lib/yang.c
@@ -255,8 +255,15 @@ struct lysc_node *yang_find_snode(struct ly_ctx *ly_ctx, const char *xpath,
 				  uint32_t options)
 {
 	struct lysc_node *snode;
+	struct ly_set *set;
+	LY_ERR err;
 
-	snode = (struct lysc_node *)lys_find_path(ly_ctx, NULL, xpath, 0);
+	err = lys_find_xpath(ly_native_ctx, NULL, xpath, options, &set);
+	if (err || !set->count)
+		return NULL;
+
+	snode = set->snodes[0];
+	ly_set_free(set, NULL);
 
 	return snode;
 }
