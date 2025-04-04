d8166a99f12277f8cf2a6ad11ea3853c137ee554
Liam Brady <lbrady@labn.net>
2025-04-04 08:43:54 -0400

    minor test fixup
    Signed-off-by: Liam Brady <lbrady@labn.net>

--- tests/lib/test_frr_socket.c
+++ tests/lib/test_frr_socket.c
@@ -11,49 +11,12 @@
 #include "test_tcp_frr_socket.h"
 
 struct frr_pthread *pth_shared;
-struct frr_pthread_attr shared = {};
-_Atomic int ctr1, ctr2;
+_Atomic bool ready;
 
-void frr_socket_test_insert_delete(void);
-void frr_socket_test_async_insert_delete(void);
-void *insert_delete_repeat(void *arg);
-
-int main(int argc, char **argv)
-{
-
-	printf("Starting\n");
-	frr_pthread_init();
-	shared.start = frr_pthread_attr_default.start;
-	shared.stop = frr_pthread_attr_default.stop;
-        pth_shared = frr_pthread_new(&shared, "FRR socket test pthread", "frrsock shared");
-	frr_pthread_run(pth_shared, NULL);
-	frr_pthread_wait_running(pth_shared);
-	rcu_read_unlock();
-
-	printf("Setting up FRR socket library\n");
-	assert(frr_socket_lib_init(pth_shared->master) == 0);
-
-	/* XXX Test explanations */
-	frr_socket_test_insert_delete();
-
-	frr_socket_test_async_insert_delete();
-
-	printf("Cleaning up FRR socket library\n");
-	frr_socket_lib_finish();
-
-	printf("Finishing\n");
-	frr_pthread_stop(pth_shared, NULL);
-	frr_pthread_finish();
-	rcu_read_lock();
-	rcu_shutdown();
-
-	printf("Done\n");
-	return 0;
-}
-
-
-/* Test the basic insertion and deletion from the FRR socket table */
-void frr_socket_test_insert_delete(void)
+/*
+ * Test the basic frr_socket_entry insertion and deletion routines within the hash table.
+ */
+static void test_socket_insert_delete(void)
 {
 	struct frr_socket_entry search_entry = {};
 	int fd, rv;
@@ -93,9 +56,9 @@ void *insert_delete_repeat(void *arg)
 {
 	struct frr_socket_entry search_entry = {};
 	struct rcu_thread *rcu_thr = arg;
-	int fd, rv;
+	int fd;
 
-	while (ctr1 < 1);;
+	while (!ready);;
 
 	rcu_thread_start(rcu_thr);
 	rcu_read_unlock();
@@ -112,8 +75,7 @@ void *insert_delete_repeat(void *arg)
 		}
 		{
 			struct frr_socket_entry *scoped_entry;
-			rv = frr_close(fd);
-			assert(rv == 0);
+			assert(frr_close(fd) == 0);
 			frr_socket_table_find(&frr_socket_table, &search_entry, scoped_entry);
 			assert(scoped_entry == NULL);
 		}
@@ -124,18 +86,19 @@ void *insert_delete_repeat(void *arg)
 
 
 /*
- * Test insertion and deletion from multiple threads
+ * Test the insertion and deletion routines when in use by multiple threads
  *
- * This is not a demonstration of realistic usage of the FRR socket abstraction, but a demonstration
- * of thread safety.
-*/
+ * This is not a demonstration of realistic usage of the FRR socket abstraction, since both the
+ * number of active threads and number of modifications to the hash table are unexpected. It is
+ * instead a demonstration of thread-safety in edge case scenarios.
+ */
 #define NUM_THREADS 4
-void frr_socket_test_async_insert_delete(void)
+void test_socket_insert_delete_async(void)
 {
 	pthread_t pthr[NUM_THREADS];
 	struct rcu_thread *rcu_thr[NUM_THREADS];
 
-	ctr1 = 0;
+	ready = false;
 	printf("Testing multi-threaded insertion and deletion\n");
 	rcu_read_lock();
 	for (int i = 0; i < NUM_THREADS; i++) {
@@ -145,9 +108,48 @@ void frr_socket_test_async_insert_delete(void)
 	rcu_read_unlock();
 
 	/* Give the go ahead for threads to start */
-	ctr1 = 1;
+	ready = true;
 
 	for (int i = 0; i < NUM_THREADS; i++) {
 		pthread_join(pthr[i], NULL);
 	}
+}
+
+
+void (*tests[])(void) = {
+	test_socket_insert_delete,
+	test_socket_insert_delete_async,
+};
+
+
+int main(int argc, char **argv)
+{
+	struct frr_pthread_attr shared = {.start = frr_pthread_attr_default.start;
+	.stop = frr_pthread_attr_default.stop;
+}
+
+printf("Starting\n");
+frr_pthread_init();
+pth_shared = frr_pthread_new(&shared, "FRR socket shared pthread", "frrsock shared");
+frr_pthread_run(pth_shared, NULL);
+frr_pthread_wait_running(pth_shared);
+rcu_read_unlock();
+
+printf("Setting up FRR socket library\n");
+assert(frr_socket_lib_init(pth_shared->master) == 0);
+
+for (unsigned int i = 0; i < array_size(tests); i++)
+	tests[i]();
+
+printf("Cleaning up FRR socket library\n");
+frr_socket_lib_finish();
+
+printf("Finishing\n");
+frr_pthread_stop(pth_shared, NULL);
+frr_pthread_finish();
+rcu_read_lock();
+rcu_shutdown();
+
+printf("Done\n");
+return 0;
 }
